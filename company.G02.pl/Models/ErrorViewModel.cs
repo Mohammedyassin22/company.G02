95d9345ff9dc3a77fda3347af1b27ee4ed72237a
mohamed yassin <mohamed yassin@DESKTOP-DL1H54K>
2025-03-10 03:44:41 +0200

    Add project files.

---
+++ company.G02.pl/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace company.G02.pl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
