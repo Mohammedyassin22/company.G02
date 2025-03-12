38a4dbe775806230252d889f73371a3fdb1bbe8e
Mohammedyassin22 <moyassin2004522@gmail.com>
2025-03-12 17:01:57 +0200

    create create action

--- company.G02.pl/Controllers/DeptController.cs
+++ company.G02.pl/Controllers/DeptController.cs
@@ -1,4 +1,6 @@
-﻿using Company.G02.BLL;
+﻿using company.G02.pl.Models;
+using Company.G02.BLL;
+using Company.G02.DAL.Modles;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 
@@ -16,6 +18,30 @@ namespace company.G02.pl.Controllers
         {
            var model= _deptReposit.getall();
             return View(model);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateDeptDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                var dept = new Department()
+                {
+                    code=dto.code,
+                    name=dto.name,
+                    createat=dto.createat
+                };
+                var count=_deptReposit.Add(dept);
+                if(count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View();
         }
     }
 }
